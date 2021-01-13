3fd69c1247a256f74bc76b7e0b1f30791061904e
bukiedow <dowbukie@gmail.com>
2021-01-13 20:08:20 +0000

    added javascript to make menu responsive, added text to modal , linked footer link

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,52 +1,61 @@
-/*show menu
-const mobileMenu = () => {
-  menu.classList.toggle('is-active');
+const menu = document.querySelector('#calvary-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+const navLogo = document.querySelector('#navbar__logo');
+
+// Display Mobile Menu
+const calvaryMenu = () => {
+  menu.classList.toggle('yes-active');
   menuLinks.classList.toggle('active');
 };
 
-menu.addEventListener('click', mobileMenu);
-
-//  Close mobile Menu when clicking on a menu item
-const hideMobileMenu = () => {
-  const menuBars = document.querySelector('.is-active');
-  if (window.innerWidth <= 768 && menuBars) {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.remove('active');
-  }
-};
-
-menuLinks.addEventListener('click', hideMobileMenu);
-navLogo.addEventListener('click', hideMobileMenu);
+menu.addEventListener('click', calvaryMenu);
 
 // Show active menu when scrolling
 const highlightMenu = () => {
   const elem = document.querySelector('.highlight');
-  const homeMenu = document.querySelector('#home-page');
-  const aboutMenu = document.querySelector('#about-page');
-  const servicesMenu = document.querySelector('#services-page');
+  const homeMenu = document.querySelector('#about-page');
+  const aboutMenu = document.querySelector('#search-page');
+  const servicesMenu = document.querySelector('#help-page');
   let scrollPos = window.scrollY;
+  // console.log(scrollPos);
 
-  /*adds 'highlight' class to my menu items
-  if (window.innerWidth > 960 && scrollPos < 600) {
-    homeMenu.classList.add('highlight');
-    aboutMenu.classList.remove('highlight');
-    return;
-  } else if (window.innerWidth > 960 && scrollPos < 1400) {
+  // adds 'highlight' class to my menu items
+  if (window.innerWidth > 960 && scrollPos < 700) {
     aboutMenu.classList.add('highlight');
-    homeMenu.classList.remove('highlight');
-    servicesMenu.classList.remove('highlight');
+    searchMenu.classList.remove('highlight');
     return;
-  } else if (window.innerWidth > 960 && scrollPos < 2345) {
-    servicesMenu.classList.add('highlight');
+  } else if (window.innerWidth > 960 && scrollPos < 1300) {
+    searchMenu.classList.add('highlight');
     aboutMenu.classList.remove('highlight');
+    helpMenu.classList.remove('highlight');
+    return;
+  } else if (window.innerWidth > 960 && scrollPos < 2300) {
+    aboutMenu.classList.add('highlight');
+    searchMenu.classList.remove('highlight');
     return;
   }
 
-  if ((elem && window.innerWIdth < 960 && scrollPos < 600) || elem) {
+  if ((elem && window.innerWIdth < 960 && scrollPos < 650) || elem) {
     elem.classList.remove('highlight');
   }
 };
 
 window.addEventListener('scroll', highlightMenu);
 window.addEventListener('click', highlightMenu);
-*/
+
+//  Close mobile Menu when clicking on a menu item
+const hideCalvaryMenu = () => {
+  const menuBars = document.querySelector('.yes-active');
+  if (window.innerWidth <= 768 && menuBars) {
+    menu.classList.toggle('yes-active');
+    menuLinks.classList.remove('active');
+  }
+};
+
+menuLinks.addEventListener('click', hideCalvaryMenu);
+navLogo.addEventListener('click', hideCalvaryMenu);
+
+
+
+
+/*for maps to span entire page before results are listed*/
